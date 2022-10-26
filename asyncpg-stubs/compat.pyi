7dd93e3f68b8dc698543e75a2e935b8e8fb3ad36
Bryan Forbes <bryan@reigndropsfall.net>
2022-10-26 17:35:20 -0500

    Changes for asyncpg 0.27.0

--- asyncpg-stubs/compat.pyi
+++ asyncpg-stubs/compat.pyi
@@ -1,18 +1,14 @@
-from asyncio import AbstractEventLoop, Future, StreamWriter, Task
+from asyncio import Future, StreamWriter
 from collections.abc import Awaitable
 from pathlib import Path
-from typing import Any, TypeVar
+from typing import TypeVar
 from typing_extensions import Final
 
 _T = TypeVar('_T')
 
-PY_37: Final[bool]
 SYSTEM: Final[str]
 
 def get_pg_home_directory() -> Path | None: ...
-def current_asyncio_task(
-    loop: AbstractEventLoop | None,
-) -> Task[Any] | None: ...
 async def wait_closed(stream: StreamWriter) -> None: ...
 async def wait_for(
     fut: Future[_T] | Awaitable[_T],
